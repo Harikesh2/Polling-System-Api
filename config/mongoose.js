1642a37d4c356523b46f543f27e134dacc145d8e
Harikesh2 <116285934+Harikesh2@users.noreply.github.com>
2023-08-30 10:19:19 +0530

    Update mongoose.js
    update mongoose

--- config/mongoose.js
+++ config/mongoose.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose');
 
 // Connect to the MongoDB database using mongoose.connect() method and specify the database URL and options
- mongoose.connect('mongodb://127.0.0.1:27017/Polling-System-Api' 
+ mongoose.connect('mongodb://localhost:27017/Polling-System-Api' 
 );
 
 
